883037e5d9b8b52d35387c61ab87220bd718702d
Tom Millichamp <tom@edgewords.co.uk>
2018-10-16 09:28:07 +0100

    added screenshots

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,7 +1,12 @@
-const {Before,After} = require('cucumber');
+const {BeforeAll,Before,After,Status} = require('cucumber');
 const {Builder, By, Key, until} = require('selenium-webdriver');
 require('chromedriver'); 
 var {setDefaultTimeout} = require('cucumber');
+
+BeforeAll(function() {
+    // Set the Cucumber Steps timeout (default is only 5s)
+    setDefaultTimeout(30 * 1000);
+});
 
 Before(async function() {
   driver = await new Builder().forBrowser('chrome').build();
@@ -10,12 +15,16 @@ Before(async function() {
       .setTimeouts( { implicit: 20000, 
       pageLoad: 60000, 
       script: 20000 } ); 
-  
-  // Set the Cucumber Steps timeout (default is only 5s)
-  setDefaultTimeout(30 * 1000);
 });
 
-After(async function() {
+After(async function(testCase) {
+  //If the Scenario Fails, Take a Screenshot and attach to Results
+  var world = this;
+  if (testCase.result.status === Status.FAILED) {
+      var screenShot = await driver.takeScreenshot();
+      // screenShot is a base-64 encoded PNG
+      world.attach(screenShot, 'image/png');
+      }
   return await driver.quit(); 
 });
 
