1ffff31f01910cd834049132c5aee9ffb808783c
Tom Millichamp <tom@edgewords.co.uk>
2018-10-22 22:55:55 +0100

    updated assertion

--- features/step_definitions/simple-steps.js
+++ features/step_definitions/simple-steps.js
@@ -1,19 +1,18 @@
-const { Given, When, Then,skipped } = require('cucumber')
-const { expect } = require('chai')
-const {By, Key, until} = require('selenium-webdriver');
+const { Given, When, Then, skipped } = require('cucumber')
+const { expect, assert } = require('chai')
+const { By, Key, until } = require('selenium-webdriver');
 
-Given('I am on the Google web page', async function () { 
-    await driver.get('https://www.google.co.uk'); 
+Given('I am on the Google web page', async function () {
+    await driver.get('https://www.google.co.uk');
 });
 
-When('I search for {string}', async function (searchString) { 
+When('I search for {string}', async function (searchString) {
     await driver.findElement(By.name("q")).click();
     await driver.findElement(By.name("q")).sendKeys(searchString + Key.RETURN);
 });
 
 Then('{string} appears in the results', async function (searchString) {
-    var bodyText = await driver.findElement(By.tagName('Body')).getText();
-    expect(bodyText).to.include(searchString);
+    assert.isTrue(await driver.findElement(By.partialLinkText(searchString)).isDisplayed());
 });
 
 Then('I see in the results:', async function (dataTable) {
@@ -21,9 +20,9 @@ Then('I see in the results:', async function (dataTable) {
     data = dataTable.hashes();
     // returns has map of table with key, value pairs
     // [ { field: 'name', content: 'edgewords' },...]
-    for (var i = 0; i < data.length; i++){
+    for (var i = 0; i < data.length; i++) {
         console.log(data[i].url);
         var bodyText = await driver.findElement(By.tagName('Body')).getText();
         expect(bodyText).to.include(data[i].url);
     }
-  });
+});
--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,31 +1,33 @@
-const {BeforeAll,Before,After,Status} = require('cucumber');
-const {Builder, By, Key, until} = require('selenium-webdriver');
-require('chromedriver'); 
-var {setDefaultTimeout} = require('cucumber');
+const { BeforeAll, Before, After, Status } = require('cucumber');
+const { Builder, By, Key, until } = require('selenium-webdriver');
+require('chromedriver');
+var { setDefaultTimeout } = require('cucumber');
 
-BeforeAll(function() {
+BeforeAll(function () {
     // Set the Cucumber Steps timeout (default is only 5s)
     //setDefaultTimeout(30 * 1000); // How do you turn the timeout off?
     setDefaultTimeout(-1);
 });
 
-Before(async function() {
-  driver = await new Builder().forBrowser('chrome').build();
+Before(async function () {
+    driver = await new Builder().forBrowser('chrome').build();
 
-  await driver.manage()
-      .setTimeouts( { implicit: 20000, 
-      pageLoad: 60000, 
-      script: 20000 } ); 
+    await driver.manage()
+        .setTimeouts({
+            implicit: 20000,
+            pageLoad: 60000,
+            script: 20000
+        });
 });
 
-After(async function(testCase) {
-  //If the Scenario Fails, Take a Screenshot and attach to Results
-  var world = this;
-  if (testCase.result.status === Status.FAILED) {
-      var screenShot = await driver.takeScreenshot();
-      // screenShot is a base-64 encoded PNG
-      world.attach(screenShot, 'image/png');
-      }
-  return await driver.quit(); 
+After(async function (testCase) {
+    //If the Scenario Fails, Take a Screenshot and attach to Results
+    var world = this;
+    if (testCase.result.status === Status.FAILED) {
+        var screenShot = await driver.takeScreenshot();
+        // screenShot is a base-64 encoded PNG
+        world.attach(screenShot, 'image/png');
+    }
+    return await driver.quit();
 });
 
